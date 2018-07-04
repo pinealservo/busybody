72c0b2fd71450c476600deaba325dd9eabc4908f
Levi Pearson <levipearson@gmail.com>
2018-07-04 13:11:21 -0600

    Read ActivitySpec from a JSON string in exe
    This also conditionally compiles different unix and windows ActivitySpec vectors
    based on where it's built.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate busybody;
 
 use busybody::activity::*;
 use busybody::native;
-use std::io::Result;
+use std::io::{Result, Error, ErrorKind};
 
 fn main() -> Result<()> {
     let mut cmdargs = std::env::args();
@@ -20,53 +20,142 @@ fn main() -> Result<()> {
 
     println!("Context: {:?}", ctx);
 
-    let mut acts: Vec<spec::ActivitySpec> = vec![];
+    #[cfg(windows)]
+    let actspec_json = r#"[
+  {
+    "Process": {
+      "name": "ipconfig",
+      "args": ["/all"]
+    }
+  },
+  {
+    "File": {
+      "path": "C:\\tmp\\foo",
+      "action": {"operation": "Create", "filetype": "Directory"}
+    }
+  },
+  {
+    "File": {
+      "path": "C:\\tmp\\foo\\bar.txt",
+      "action": {"operation": "Create", "filetype": "File"}
+    }
+  },
+  {
+    "File": {
+      "path": "C:\\tmp\\foo\\bar.txt",
+      "action": {"operation": "Update", "filetype": "File"
+      }
+    }
+  },
+  {
+    "File": {
+      "path": "C:\\tmp\\foo\\bar.txt",
+      "action": {"operation": "Delete", "filetype": "File"}
+    }
+  },
+  {
+    "File": {
+      "path": "C:\\tmp\\foo",
+      "action": {"operation": "Delete", "filetype": "Directory"}
+    }
+  },
+  {
+    "Network": {
+      "dest": "192.168.0.1:80",
+      "src": "192.168.0.134",
+      "proto": "TCP",
+      "size": 42
+    }
+  },
+  {
+    "Network": {
+      "dest": "192.168.0.1:8000",
+      "src": "192.168.0.134",
+      "proto": "UDP",
+      "size": 42
+    }
+  }
+]"#;
 
-    acts.push(spec::ActivitySpec::Process{
-        name: "ls".to_string(),
-        args: vec!["-l".to_string(), "/tmp".to_string()],
-    });
-    acts.push(spec::ActivitySpec::File{
-        path: ["/", "tmp", "foo"].into_iter().collect(),
-        action: spec::FileAction{ operation: spec::FileOp::Create, filetype: spec::FileType::Directory },
-    });
-    acts.push(spec::ActivitySpec::File{
-        path: ["/", "tmp", "foo", "bar.txt"].into_iter().collect(),
-        action: spec::FileAction{ operation: spec::FileOp::Create, filetype: spec::FileType::File },
-    });
-    acts.push(spec::ActivitySpec::File{
-        path: ["/", "tmp", "foo", "bar.txt"].into_iter().collect(),
-        action: spec::FileAction{ operation: spec::FileOp::Update, filetype: spec::FileType::File },
-    });
-    acts.push(spec::ActivitySpec::File{
-        path: ["/", "tmp", "foo", "bar.txt"].into_iter().collect(),
-        action: spec::FileAction{ operation: spec::FileOp::Delete, filetype: spec::FileType::File },
-    });
-    acts.push(spec::ActivitySpec::File{
-        path: ["/", "tmp", "foo"].into_iter().collect(),
-        action: spec::FileAction{ operation: spec::FileOp::Delete, filetype: spec::FileType::Directory },
-    });
-    acts.push(spec::ActivitySpec::Network{
-        dest: "192.168.0.1:80".parse().unwrap(),
-        src: "192.168.0.103".parse().unwrap(),
-        proto: spec::IpProto::TCP,
-        size: 42,
-    });
-    acts.push(spec::ActivitySpec::Network{
-        dest: "192.168.0.1:8000".parse().unwrap(),
-        src: "192.168.0.103".parse().unwrap(),
-        proto: spec::IpProto::UDP,
-        size: 42,
-    });
+    #[cfg(unix)]
+    let actspec_json = r#"[
+  {
+    "Process": {
+      "name": "ls",
+      "args": ["-l", "/tmp"]
+    }
+  },
+  {
+    "File": {
+      "path": "/tmp/foo",
+      "action": {"operation": "Create", "filetype": "Directory"}
+    }
+  },
+  {
+    "File": {
+      "path": "/tmp/foo/bar.txt",
+      "action": {"operation": "Create", "filetype": "File"}
+    }
+  },
+  {
+    "File": {
+      "path": "/tmp/foo/bar.txt",
+      "action": {"operation": "Update", "filetype": "File"
+      }
+    }
+  },
+  {
+    "File": {
+      "path": "/tmp/foo/bar.txt",
+      "action": {"operation": "Delete", "filetype": "File"}
+    }
+  },
+  {
+    "File": {
+      "path": "/tmp/foo",
+      "action": {"operation": "Delete", "filetype": "Directory"}
+    }
+  },
+  {
+    "Network": {
+      "dest": "192.168.0.1:80",
+      "src": "192.168.0.134",
+      "proto": "TCP",
+      "size": 42
+    }
+  },
+  {
+    "Network": {
+      "dest": "192.168.0.1:8000",
+      "src": "192.168.0.134",
+      "proto": "UDP",
+      "size": 42
+    }
+  }
+]"#;
+
+    let actspec_res: std::result::Result<Vec<spec::ActivitySpec>,serde_json::Error> =
+        serde_json::from_str(actspec_json);
+    let actspec;
+
+    match actspec_res {
+        Ok(spec) => actspec = spec,
+        Err(e)   => {
+            println!("Error decoding ActivitySpec from JSON: {:?}", e);
+            return Err(Error::new(ErrorKind::Other, "Bad ActivitySpec".to_string()));
+        }
+    }
 
     let mut logs: Vec<log::ActivityLog> = vec![];
-    for act in &acts {
-        println!("\nActivitySpec: {:?}\n", act);
+    for act in &actspec {
+        println!("Executing ActivitySpec: {:?}", act);
         let entry = act.execute(&ctx)?;
         logs.push(entry);
     }
 
-    let jstr = serde_json::to_string(&logs).unwrap();
+    println!("\nComplete logs as a JSON Array:");
+
+    let jstr = serde_json::to_string_pretty(&logs).unwrap();
     println!("{}", jstr);
 
     Ok(())
